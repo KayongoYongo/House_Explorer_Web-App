be6ffcd938cbc9fa7d7bd1db1fc76e3a45db00df
KayongoYongo <sknsammie10@gmail.com>
2023-05-30 15:40:16 +0000

    updated the ID column to display in UUID format

--- models/house.py
+++ models/house.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.dialects.mysql import CHAR
+import uuid
 
 # Create the flask application object
 app = Flask(__name__)
@@ -16,7 +18,7 @@ db = SQLAlchemy(app)
 
 class House(db.Model):
     __tablename__ = 'houses'
-    id = Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(CHAR(36), default=str(uuid.uuid4()), primary_key=True, unique=True, nullable=False)
     created_at = Column(db.DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(db.DateTime, default=datetime.utcnow(), nullable=False)
     name = db.Column(db.String(128), nullable=False)

1cf6a1b35b9aebcd8a1bb538e35a84b508f27263
KayongoYongo <sknsammie10@gmail.com>
2023-06-06 02:42:36 +0300

    updated the user to end USEr with CRU privileges

--- views/house.py
+++ views/house.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from models.house import House
+from views import app_views
 
 # Create the flask application object
 app = Flask(__name__)
@@ -10,15 +11,18 @@ app = Flask(__name__)
 # This establishes a connection between the database and the flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://end_user:password@localhost:3306/house_hunter'
 
+# Set the track modifications option to False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Create the db object
 db = SQLAlchemy(app)
 
 # Create a route to handle form submission
-@app.route('/house', methods=['POST'])
+@app_views.route('/house', methods=['POST'])
 def create_house():
     # Get form data
-    name = request.form['name']
-    email = request.form['email']
+    name = request.form.get('name')
+    email = request.form.get('email')
 
     # Create an instance of the model
     form_data = House(name=name, email=email)

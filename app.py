8ee3dddc8367fb2cd6dfb6161d76c880e8751ba6
KayongoYongo <sknsammie10@gmail.com>
2023-06-08 05:23:53 +0300

    optimized the functionality and removed unnecessary varioables in app.py

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
 from flask import Flask
-from views.routes import site_views
-from views import app_views
-from views import db
+from views import create_app
 
-# A flask application instance
-app = Flask(__name__)
-
-# This configuration establishes a connection between the database and the flask app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://end_user:password@localhost:3306/house_hunter'
-
-# Register the app_views blueprint
-app.register_blueprint(site_views, url_prefix='')
-
-#Register the views blueprint
-app.register_blueprint(app_views, url_prefix='')
-
-# Register the Flask app with SQLAlchemy
-db.init_app(app)
+# Create the app object
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)

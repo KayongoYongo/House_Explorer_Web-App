1fb6b885bef67530f0b81de3adb9daae1d1235f5
KayongoYongo <sknsammie10@gmail.com>
2023-06-09 06:38:53 +0300

    I updated it with the proper functions for log in

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
 
 # Creating a database object
 db = SQLAlchemy()
@@ -11,18 +12,36 @@ def create_app():
     # This configuration establishes a connection between the database and the flask app
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://end_user:password@localhost:3306/house_hunter'
 
+    # This provides session security and protect against tampering of cookies and other data
+    app.config['SECRET_KEY'] = 'bonoko and friends'
+
     # Initialize the database
     db.init_app(app)
 
+    # Import the blueprints from their modules
     from views.routes import  site_views
     from views.sign_up import users_blueprint
-    from views.house import house_blueprint
+    from views.property import property_blueprint
     from views.log_in import login_blueprint
+    from views.log_out import logout_blueprint
 
     # Register the blueprints
     app.register_blueprint(site_views, url_prefix='')
     app.register_blueprint(users_blueprint, url_prefix='')
-    app.register_blueprint(house_blueprint, url_prefix='')
+    app.register_blueprint(property_blueprint, url_prefix='')
     app.register_blueprint(login_blueprint, url_prefix='')
+    app.register_blueprint(logout_blueprint, url_prefix='')
+
+    # Create an instance of LoginManager
+    login_manager = LoginManager()
+    # page to redirect if user is not logged in
+    login_manager.login_view = 'site_views.log_in'
+    # Initialize the login manager
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        from models.users import User
+        return User.query.get(id)
 
     return app

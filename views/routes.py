fcc5185d77f9b6e24aa494f017353a9dc334b17b
KayongoYongo <sknsammie10@gmail.com>
2023-06-09 06:34:06 +0300

    updates the routes so that some of them can be available when the user logs in

--- views/routes.py
+++ views/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 # Create a blueprint to handle different routes within the web page
 site_views = Blueprint('site_views', __name__, static_folder='static', template_folder='templates', static_url_path='/static')
@@ -6,20 +7,21 @@ site_views = Blueprint('site_views', __name__, static_folder='static', template_
 @site_views.route('/')
 @site_views.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', user=None)
 
 @site_views.route('/houses')
+@login_required
 def house_page():
-    return render_template('House.html')
+    return render_template('House.html', user=current_user)
 
 @site_views.route('/about')
 def about_page():
-    return render_template('about.html')
+    return render_template('about.html', user=None)
 
 @site_views.route('/sign_up')
 def sign_page():
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=None)
 
 @site_views.route('/log_in')
 def log_in():
-    return render_template('log_in.html')
+    return render_template('log_in.html', user=None)

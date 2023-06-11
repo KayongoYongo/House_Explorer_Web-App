7240cdd0e6ff8530595128ef56ce4a1108a691ab
KayongoYongo <sknsammie10@gmail.com>
2023-06-11 06:15:03 +0300

    Added a primary and foreign key relationship to the tables

--- models/property.py
+++ models/property.py
@@ -3,7 +3,8 @@
 from datetime import datetime
 from flask import Flask
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String, MetaData
+from models.users import User
+from sqlalchemy import Column, Integer, ForeignKey, String, MetaData
 from views import db
 import uuid
 
@@ -16,7 +17,7 @@ To create the model, carry out the following command  within the file's director
 Withing the flask console:
 >>>from models.property import db, Property
 >>>db.create_all()
-"""
+
 # Create the flask application object
 app = Flask(__name__)
 
@@ -25,16 +26,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://end_user:password@local
 
 # Initialize the SQLAlchemy extension
 db.init_app(app)
+"""
 
-class Property(db.Model, UserMixin):
-    __tablename__ = 'houses'
+class Property(db.Model):
+    __tablename__ = 'property'
     id = Column(db.String(60), primary_key=True, nullable=False)
     created_at = Column(db.DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(db.DateTime, default=datetime.utcnow(), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     cost = db.Column(db.Integer(), nullable=False)
-    Rooms = db.Column(db.Integer(), nullable=False)
-    Location = db.Column(db.String(128), nullable=False)
+    rooms = db.Column(db.Integer(), nullable=False)
+    county = db.Column(db.String(128), nullable=False)
     estate = db.Column(db.String(128), nullable=False)
     electricity = db.Column(db.String(128), nullable=False)
     water_availability = db.Column(db.String(128), nullable=False)
@@ -42,14 +44,14 @@ class Property(db.Model, UserMixin):
     parking = db.Column(db.String(128), nullable=False)
     security = db.Column(db.String(128), nullable=False)
     garbage_collection = db.Column(db.String(128), nullable=False)
-    photo = db.Column(db.LargeBinary, nullable=False)
+    photo = db.Column(String(255), nullable=False)
 
-    def __init__(self, email, cost, rooms, location, estate, water_availability, internet_provider, parking, security, garbage_collection, electricity, photo):
+    def __init__(self, user_id, cost, rooms, county, estate, water_availability, internet_provider, parking, security, garbage_collection, electricity, photo):
         self.id = str(uuid.uuid4())
-        self.email = email
+        self.user_id = user_id
         self.cost = cost
         self.rooms = rooms
-        self.location = location
+        self.county = county
         self.estate = estate
         self.water_availability = water_availability
         self.internet_provider = internet_provider
